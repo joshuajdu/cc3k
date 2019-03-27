e4c9042146da3bceebea34f5b416879389102758
Jonathan Lock-Ming Poon <jlpoon@uwaterloo.ca>
2019-03-27 14:56:26 -0400

    Upload new file

---
+++ Human.cpp
@@ -0,0 +1,19 @@
+#include "Human.h"
+#include <string>
+
+using namespace std;
+
+Human::Human()
+{
+    //ctor
+    hp = 140;
+    attack = 20;
+    defence = 20;
+    race = "human";
+
+}
+
+Human::~Human()
+{
+    //dtor
+}
