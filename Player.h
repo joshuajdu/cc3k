02cc14e24f2b553989c5a649c48efb8eb4035f07
Jonathan Lock-Ming Poon <jlpoon@uwaterloo.ca>
2019-03-28 14:03:40 -0400

    Replace Player.h

--- Player.h
+++ Player.h
@@ -2,40 +2,76 @@
 #define PLAYER_H
 
 #include <string>
+#include <stdlib.h>
+#include <iostream>
+#include "Enemy.h"
+#include "Posn.h"
 
 using namespace std;
 
 class Player
 {
     public:
-        Player(int hp, int attack, int defence, string race);
-        ~Player();
+        Player(int hp = 140, int attack = 20, int defence = 20, string race = "Human");
 
         void Move(string direction);
-        //void CalculateDamage(Enemy &enemy);
+        void CalculateDamage(Enemy &enemy);
         //void pickupItem(const Item &item);
         //virtual void pickupTreasure(const Treasure &treasure) = 0;
         //virtual void usePotion(const Potion &potion) = 0;
 
-	int get_maxhp();
+        int get_maxhp();
         int* get_hp();
-	int get_baseatk();
-	int* get_atk();
-	int get_basedef();
-	int* get_def();
+        int get_baseatk();
+        int* get_atk();
+        int get_basedef();
+        int* get_def();
+
+        string get_race();
+        void Position();
 
     protected:
-	int maxhp;
         int hp;
-	int baseatk;
         int atk;
-	int basedef;
         int def;
+        int maxhp;
+        int baseatk;
+        int basedef;
         int gold;
-        //posn p;
+        posn p = {0,0};
         string race;
         bool barrier_suit;
         bool compass;
+
+    private:
+};
+
+class Dwarf : public Player
+{
+    public:
+        Dwarf();
+
+    protected:
+
+    private:
+};
+
+class Elf : public Player
+{
+    public:
+        Elf();
+
+    protected:
+
+    private:
+};
+
+class Orc : public Player
+{
+    public:
+        Orc();
+
+    protected:
 
     private:
 };
