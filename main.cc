12bd459c2bcd0c45a4777cfb29ffc7ed0742d75f
j94jiang <j94jiang@student.cs.uwaterloo.ca>
2019-03-28 14:50:44 -0400

    msg

--- main.cc
+++ main.cc
@@ -1,10 +1,26 @@
 #include <iostream>
 #include "Player.h"
-#include "Potions.h"
-#include "Items.h"
+#include "Potion.h"
+#include "Item.h"
 #include "Posn.h"
+#include "Treasure.h"
 
 using namespace std;
 
+void printPlayer(Player *a){
+    cout << "HP: " << *a->get_hp() << endl;
+    cout << "ATK: " << *a->get_atk() << endl;
+    cout << "DEF: " << *a->get_def() << endl;
+}
+
 int main(){
+    Player a;
+    printPlayer(&a);
+    
+    RH p1 = RH(5,6);
+    BA p2 = BA(1,2);
+    BD p3 = BD(1,0);
+    PH p4 = PH(0,0);
+    WA p5 = WA(1,1);
+    WD p6 = WD(8,2);
 }
