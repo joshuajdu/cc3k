6212095174a263f371e1bd5c0ce83f97aecd3c5f
twelvekeystrokes <twelvekeystrokes@gmail.com>
2019-04-02 15:26:14 -0400

    main

--- main.cc
+++ main.cc
@@ -1,28 +1,46 @@
 #include <iostream>
+#include <fstream>
 #include "Player.h"
 #include "Posn.h"
 #include "Enemy.h"
+#include "Item.h"
+#include "Potion.h"
+#include "Treasure.h"
+#include "Cell.h"
+#include "Chamber.h"
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+void printPlayer(Player &a){
+    cout << "HP: " <<  *a.get_hp() << endl;
+    cout << "ATK: " << *a.get_atk() << endl;
+    cout << "DEF: " << *a.get_def() << endl;
+    cout << "Gold: " << *a.get_gold() << endl;
+    cout << "BarrierSuit: " << a.get_barriersuit() << endl;
+}
 
-    Orc a;
-    cout << "I am a " << a.get_race() << endl;
-    cout << *(a.get_hp()) << endl;
+void useItem(Player &a, Item &p){
+    p.useItem(a);
+}
 
-    a.Position();
-    a.Move("ea");
-    a.Position();
-    a.Move("so");
-    a.Position();
+//Global Constant Chambers used for Generation Purposes
+Chamber a, b, c, d, e;
 
-    Goblin b;
-    cout << "I am a " << b.get_race() << endl;
-    cout << *(b.get_hp()) << endl;
+int main(int argc, char *argv[]){
+    Player player;
+    BA pot1 = BA(0,0);
+    if (argc == 2) {
+        ifstream inputFile (argv[1]);
+        string line;
+        Floor userInput;
+        int rowCount = 0;
+        while (getline(inputFile,line)) {
+            userInput.addInput(line, rowCount);
+            rowCount++;
+        }
+        inputFile.close();
+    }
+    generateDefaultChambers(a, b, c, d, e);
 
     return 0;
 }
-
