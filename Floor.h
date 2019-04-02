242a9f58d98dc8272ef10249333e2d5c489a496b
twelvekeystrokes <twelvekeystrokes@gmail.com>
2019-04-02 19:43:27 -0400

    added spawn rates

--- Floor.h
+++ Floor.h
@@ -11,6 +11,24 @@
 #include "Treasure.h"
 #include "Posn.h"
 
+struct spawnRates {
+    enum enemyRates {
+        W = 3;  // Werewolf         (4/18, 0-3)
+        V = 6;  // Vampire          (3/18, 4-6)
+        N = 11; // Goblin           (5/18, 7-11)
+        M = 13; // Merchant         (2/18, 12-13)
+        X = 15; // Phoenix          (2/18, 14-15)
+        T = 17; // Troll            (2/18, 16-17)
+        Total = 18;
+    }
+    enum goldRates {
+        G = 4;   // Gold            (5/8, 0-4)
+        G_D = 5; // Dragon Hoard    (1/8, 5-5)
+        G_S = 7; // Small Hoard     (2/8, 6-7)
+        Total = 8;
+    }
+};
+
 class Floor{
     std::vector< vector<Cell> > cells;
     std::vector<Enemy*> enemies;
