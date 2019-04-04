fe522a6fee15e75bf682dbf57d04a395193d152d
Josh Du <jj2999du@uwaterloo.ca>
2019-04-04 14:22:04 -0400

    updated main to include cmd line arguments; input

--- main.cc
+++ main.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include "Player.h"
 #include "Posn.h"
 #include "Enemy.h"
@@ -26,12 +27,22 @@ void useItem(Player &a, Item &p){
 //Global Constant Chambers used for Generation Purposes
 Chamber a, b, c, d, e;
 
-int main(){
+int main(int argc, char* argv[]){
     Player player;
     BA pot1 = BA(0,0);
     generateDefaultChambers(a, b, c, d, e);
     Floor temp;
     temp.generateFloor();
+    if (argc == 2) {
+        ifstream inputFile (argv[1]);
+        string line;
+        int rowCount = 0;
+        while (getline(inputFile,line)) {
+            temp.addInput(line, rowCount);
+            rowCount++;
+        }
+        inputFile.close();
+    }
     temp.printDisplay();
     return 0;
 }
